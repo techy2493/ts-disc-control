16ccf00704ffc441f2290cc8051e17b321f42045
Brandyn <you@example.com>
2023-12-27 17:34:40 -0600

    Fixes #1

--- src/discord.js
+++ src/discord.js
@@ -1,78 +1,97 @@
-const config = require('./config');
-const { Client, Collection, Events, GatewayIntentBits, REST, Routes } = require('discord.js');
-const commands = require('./actions/discordComamnds')
-
-
+const config = require("./config");
+const {
+  Client,
+  Collection,
+  Events,
+  GatewayIntentBits,
+  REST,
+  Routes,
+} = require("discord.js");
+const commands = require("./actions/discordComamnds");
 
 class Discord {
-    constructor() {
-        this.client = new Client({
-            intents: [
-                GatewayIntentBits.Guilds,
-                GatewayIntentBits.GuildMessages,
-                GatewayIntentBits.GuildMembers,
-                GatewayIntentBits.GuildMessageReactions
-            ],
-        });
-        
-        
-        this.client.commands = new Collection();
-        for (const command of commands) {
-            if ('data' in command && 'execute' in command) {
-                this.client.commands.set(command.data.name, command);
-            } else {
-                console.log(`[WARNING] The command ${command} is missing a required "data" or "execute" property.`);
-            }
+  constructor() {
+    this.client = new Client({
+      intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.GuildMessageReactions,
+      ],
+    });
+
+    this.client.commands = new Collection();
+    for (const command of commands) {
+      if ("data" in command && "execute" in command) {
+        this.client.commands.set(command.data.name, command);
+      } else {
+        console.log(
+          `[WARNING] The command ${command} is missing a required "data" or "execute" property.`
+        );
+      }
+    }
+  }
+
+  async connect() {
+    return new Promise((resolve, reject) => {
+      this.client.login(config.discord.token);
+      this.client.on("ready", () => {
+        this.client.intents;
+        console.log("Discord Bot Connected");
+        this.regsiterCommands();
+        resolve();
+      });
+      this.client.on(Events.InteractionCreate, async (interaction) => {
+        if (!interaction.isChatInputCommand()) return;
+
+        const command = interaction.client.commands.get(
+          interaction.commandName
+        );
+        try {
+          await command.execute(interaction);
+        } catch (error) {
+          console.error(error);
+          await interaction.reply({
+            content: "There was an error while executing this command!",
+            ephemeral: true,
+          });
         }
-    }
+      });
+    });
+  }
 
-    async connect() {
-        return new Promise((resolve, reject) => {
-            this.client.login(config.discord.token);
-            this.client.on('ready', () => {
-                this.client.intents
-                console.log('Discord Bot Connected');
-                resolve();
-            });
-            this.client.on(Events.InteractionCreate, async interaction => {
-                if (!interaction.isChatInputCommand()) return;
+  async regsiterCommands() {
+    let jsonCommands = [];
+    commands
+      .filter((x) => x.public)
+      .forEach((x) => jsonCommands.push(x.data.toJSON()));
+    let devJsonCommands = [];
+    commands
+      .filter((x) => !x.public)
+      .forEach((x) => devJsonCommands.push(x.data.toJSON()));
+    const rest = new REST({ version: "10" }).setToken(config.discord.token);
 
-                const command = interaction.client.commands.get(interaction.commandName);
-                try {
-                    await command.execute(interaction);
-                } catch (error) {
-                    console.error(error);
-                    await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-                }
+    const data = await rest.put(
+      Routes.applicationGuildCommands(
+        config.discord.clientID,
+        config.discord.guild
+      ),
+      { body: devJsonCommands }
+    );
 
-            });
-        })
-    }
+    console.log(
+      `Successfully reloaded ${data.length} dev application (/) commands.`
+    );
 
-    async regsiterCommands() {
-        let jsonCommands = [];
-        commands.filter(x => x.public ).forEach(x => jsonCommands.push(x.data.toJSON()));
-        let devJsonCommands = [];
-        commands.filter(x => !x.public ).forEach(x => devJsonCommands.push(x.data.toJSON()));
-        const rest = new REST({ version: '10' }).setToken(config.discord.token);
-        
-        const data = await rest.put(
-			Routes.applicationGuildCommands(config.discord.clientID, config.discord.guild),
-			{ body: devJsonCommands },
-		);
+    const data2 = await rest.put(
+      Routes.applicationCommands(config.discord.clientID),
+      { body: jsonCommands }
+    );
 
-        console.log(`Successfully reloaded ${data.length} dev application (/) commands.`);
-
-        const data2 = await rest.put(
-			Routes.applicationCommands(config.discord.clientID),
-			{ body:  jsonCommands},
-		);
-
-        console.log(`Successfully reloaded ${data2.length} public application (/) commands.`);
-        
-    }
-    
-    
+    console.log(
+      `Successfully reloaded ${data2.length} public application (/) commands.`
+    );
+  }
 }
 
 module.exports = new Discord();
