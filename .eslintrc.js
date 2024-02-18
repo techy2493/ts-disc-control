66dedf798bd68ec0dc2ba9a0f4c198f9faa1214b
Brandyn Abel <brandynabel@gmail.com>
2024-02-18 08:47:31 -0600

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,14 @@
 module.exports = {
   "env": {
       "browser": true,
-      "es2021": true
+      "es2021": true,
+      "amd": true,
+      "node": true
   },
   "extends": "eslint:recommended",
   "parserOptions": {
       "ecmaVersion": "latest",
       "sourceType": "module"
   },
+  
 }
